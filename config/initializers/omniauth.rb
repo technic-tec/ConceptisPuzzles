233c15286389ec58ae6769f1c2e29ed5a5b9f701
Terence Hang <technic.tec@gmail.com>
2015-11-23 18:35:06 +0800

    update omniauth and client secrets for heroku
    Add template client_secrets.json, with client_id/client_secret filled with ENV variable names.
    Update omniauth to load ENV variable after Google::APIClient::ClientSecrets.load
    fix PostgreSQL user to postgres

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,11 @@
 require 'google/api_client/client_secrets'
 
 CLIENT_SECRETS = Rails.application.config.x.CLIENT_SECRETS = Google::APIClient::ClientSecrets.load
+puts CLIENT_SECRETS.to_json
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
-    CLIENT_SECRETS.client_id,
-    CLIENT_SECRETS.client_secret, {
+    ENV[CLIENT_SECRETS.client_id],
+    ENV[CLIENT_SECRETS.client_secret], {
       :scope => ['profile', 'email'],
       :approval_prompt => "auto",
       :include_granted_scopes => true
