096ff610a725d1aa97a9e59abb1d603ae77927a9
terence <technic.tec@gmail.com>
2018-05-09 18:39:39 +0800

    fix domain for openshift3 deployment

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_conceptis_puzzles_session', domain: 'localhost'
+Rails.application.config.session_store :cookie_store, key: '_conceptis_puzzles_session', domain: '193b.starter-ca-central-1.openshiftapps.com'
